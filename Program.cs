fb4b2a968358362244caffa13f4130b95d3109e5
Sindre Aarstrand <zindre17@gmail.com>
2022-07-21 23:38:50 +0200

    Initial usable program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Monline;
+
+var api = new ApiClient();
+
+var calls = new List<Task<Pokemon?>>();
+foreach (var arg in args)
+{
+    calls.Add(api.GetPokemonAsync(arg));
+};
+
+var potentialMons = await Task.WhenAll(calls);
+
+var mons = potentialMons.OfType<Pokemon>().ToArray();
+
+Printer.PrintPokemon(mons);
+
