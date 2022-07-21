8bfc81f069d0f4911f9a1b734d0c7b98a400cfe7
Sindre Aarstrand <zindre17@gmail.com>
2022-07-21 23:21:22 +0200

    Add console row and area

---
+++ ConsoleArea.cs
@@ -0,0 +1,45 @@
+namespace Monline;
+
+public class ConsoleArea
+{
+    private readonly ConsoleRow[] lines;
+    private ConsoleOutput common = new();
+
+    public ConsoleArea(int height, int width)
+    {
+        lines = new ConsoleRow[height];
+        for (int i = 0; i < height; i++)
+        {
+            lines[i] = new(width);
+        }
+    }
+
+    public ConsoleOutput Common
+    {
+        get => common;
+        set
+        {
+            foreach (var line in lines)
+            {
+                line.Common = line.Common.Apply(value);
+            }
+            common = value;
+        }
+    }
+
+    public ConsoleRow this[int index] => lines[index];
+
+    public void Print()
+    {
+        foreach (var line in lines)
+        {
+            line.Print();
+            Console.WriteLine();
+        }
+    }
+
+    public void PrintRow(int index)
+    {
+        this[index].Print();
+    }
+}
---
+++ ConsoleRow.cs
@@ -0,0 +1,63 @@
+namespace Monline;
+
+public class ConsoleRow
+{
+    private readonly int maxWidth;
+    private readonly List<ConsoleOutput> segments = new();
+
+    private int currentWidth = 0;
+
+    public ConsoleRow(int maxWidth)
+    {
+        this.maxWidth = maxWidth;
+    }
+
+    public ConsoleOutput Common { get; set; } = new();
+
+    public void AddSegment(ConsoleOutput segment, double? widthRatio = null)
+    {
+        if (widthRatio is not null)
+        {
+            if (widthRatio is < 0 or > 1)
+            {
+                throw new ArgumentException("Must be between 0 and 1.", nameof(widthRatio));
+            }
+            segment = segment with { Width = (int)Math.Round(maxWidth * widthRatio.Value) };
+        }
+
+        var segmentWidth = segment.ActualWidth;
+        if ((segmentWidth + currentWidth) > maxWidth)
+        {
+            throw new InvalidOperationException("Cannot add segment. Max width will be violated.");
+        }
+
+        currentWidth += segmentWidth;
+        segments.Add(segment);
+    }
+
+    public void Fill(ConsoleOutput row)
+    {
+        if (segments.Any())
+        {
+            throw new InvalidOperationException("Cannot fill non-empty row.");
+        }
+
+        currentWidth = maxWidth;
+        row = row with { Width = maxWidth };
+
+        segments.Add(row);
+    }
+
+    public void Print()
+    {
+        foreach (var segment in segments)
+        {
+            segment.Apply(Common).Print();
+        }
+        if (currentWidth < maxWidth)
+        {
+            var rest = Common with { Width = maxWidth - currentWidth };
+            rest.Print();
+        }
+    }
+}
