cc3bb0758d988546e88bf407d0ead7db6f2c87ef
Sindre Aarstrand <zindre17@gmail.com>
2022-08-11 22:50:24 +0200

    Add xml doc to TextAlignment

--- Fansi/TextAlignment.cs
+++ Fansi/TextAlignment.cs
@@ -1,8 +1,20 @@
 namespace Fansi;
 
+/// <summary>
+///     All possible text alignments.
+/// </summary>
 public enum TextAlignment
 {
+    /// <summary>
+    ///     Centered alignment.
+    /// </summary>
     Center,
+    /// <summary>
+    ///     Left alignment.
+    /// </summary>
     Left,
+    /// <summary>
+    ///     Right alignment.
+    /// </summary>
     Right
 }
